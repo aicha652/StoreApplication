4c57aff840b40678361746d5c906e745e3a799e5
aicha652 <aichaazlf652@gmail.com>
2024-01-11 17:08:33 +0100

    admin file

--- models/admin.py
+++ models/admin.py
@@ -1,5 +1,5 @@
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Boolean, String
 from os import getenv
 
 class User(BaseModel, Base):
@@ -9,6 +9,7 @@ class User(BaseModel, Base):
         username = Column(String(80), unique=True, nullable=False)
         email = Column(String(120), unique=True, nullable=False)
         password = Column(String(180), unique=False, nullable=False)
+        is_admin = Column(Boolean, default=False)
     else:
         name = ''
         username = ''
