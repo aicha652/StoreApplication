b12b98fe7679c90d36b60f5d37dd9df04a5169af
aicha652 <aichaazlf652@gmail.com>
2024-01-14 14:59:56 +0100

    commit

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
 from flask import Flask
+from models import *
 
 app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def products_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    products = storage.all("Product").values()
+    return render_template('products_list.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
