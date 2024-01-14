65dc78a0e215ab65e7c8ec233698f97cd6104dcf
aicha652 <aichaazlf652@gmail.com>
2024-01-14 01:45:34 -0800

    mysql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS Ecommercedb;
-CREATE USER IF NOT EXISTS 'aicha'@'localhost' IDENTFIED BY 'aicha_admin';
+CREATE USER IF NOT EXISTS 'aicha'@'localhost' IDENTIFIED BY 'aicha_admin';
 GRANT ALL PRIVILEGES ON Ecommercedb .* TO 'aicha'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'aicha'@'localhost';
 FLUSH PRIVILEGES;
